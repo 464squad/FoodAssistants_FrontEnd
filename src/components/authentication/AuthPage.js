c7b0a32c38d39dec1c0a09d28eda994271512e37
Cameron Flowers <cameron.flowers13@gmail.com>
2022-12-01 02:10:15 +0200

    Adding AuthPage and Login components

---
+++ src/components/authentication/AuthPage.js
@@ -0,0 +1,5 @@
+function AuthPage() {
+  return <div>This is my soon to be AuthPage!</div>
+}
+
+export default AuthPage
---
+++ src/components/authentication/Login.js
@@ -0,0 +1,5 @@
+function Login() {
+  return <div>This should be our Login component!</div>
+}
+
+export default Login
