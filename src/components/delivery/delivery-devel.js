7ed863b94fa6429651ba6aa9c23e94115d5b7864
Zahra Khan <zahrakhanniazi@gmail.com>
2022-12-01 23:18:24 -0500

    updating the delievery-pageComponent

--- src/components/delivery/delivery-devel.js
+++
@@ -1,5 +0,0 @@
-function delivery_devel() {
-    return <div>This should be our delivery component!</div>
-  }
-  
-  export default delivery_devel
---
+++ src/components/delivery/delivery-pageComponent.js
@@ -0,0 +1,5 @@
+function delivery_page() {
+    return <div>This should be our delivery page component!</div>
+  }
+  
+  export default delivery_page
