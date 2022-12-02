8e7b440081d6de6078ce3495b90b14af1ab4987c
malalatiana <malalatiana.rasedoara@lc.cuny.edu>
2022-12-02 17:30:39 -0500

    adding front end submission page component

---
+++ src/components/submission/submission_page_component.js
@@ -0,0 +1,9 @@
+function submission_page_component(){
+    return (
+        <div className= "submission_page_component">
+            <h1> Here is the submission page component</h1>
+        </div>
+    )
+}
+
+export default submission_page_component;
