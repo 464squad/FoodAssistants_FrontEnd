1355020b3a7079922245205dd2b2a3cf8376ca6c
jamarikelly <107642971+jamarikelly@users.noreply.github.com>
2022-12-04 23:16:23 -0500

    Add files via upload

---
+++ src/components/submission/submissionForm.js
@@ -0,0 +1,9 @@
+function submission_form_component(){
+    return (
+        <div className= "submission_form_component">
+            <h1> Here is the submission form component</h1>
+        </div>
+    )
+}
+
+export default submission_form_component;
