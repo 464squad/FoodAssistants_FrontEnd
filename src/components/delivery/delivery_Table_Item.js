729ee332ecc557eb228aa90a4a96c6cb3124afc8
Kevin Savinovich <dev.savinovich@gmail.com>
2022-12-05 00:43:35 -0500

    created the delivery item components

---
+++ src/components/delivery/delivery_Table_Item.js
@@ -0,0 +1,9 @@
+function delivery_Table_Item(){
+    return (
+        <div className= "delivery_Table_Item">
+            <h1> This is the delivery item table</h1>
+        </div>
+    )
+}
+
+export default delivery_Table_Item;
