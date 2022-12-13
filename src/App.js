0acdd6bdeb6425df5b3e801c7accebc801c9d818
Kevin Savinovich <dev.savinovich@gmail.com>
2022-12-12 19:16:33 -0500

    fixing the heading and columns

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import React from 'react';
 
 const App =() =>{
   return (
+    
     <div className="app-container">
+      <h1> Delivery Table</h1>
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-            <th>Status</th>
+            <th>Name, # in Home</th>
+            <th>Grocery Items</th>
+            <th>General Hygiene</th>
+            <th>Feminine Hygiene</th>
+            <th>Cleaning/Health Supplies</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             {/* this is where the data goes */}
-            <td>Jenny</td>
-            <td>plum street</td>
-            <td>800-888-8888</td>
-            <td>h=jenny1@gmail.com</td>
-            
+            <td>Lameisha 3 people in house hold</td>
+            <td>chicken (1x)</td>
+            <td>Deodorant(3x)</td>
+            <td>Feminine Wipes (1x)</td>
+            <td>All purpose Cleaner (1x)</td>
           </tr>
         </tbody>
       </table>
      
-        <h1>test run</h1>
+      
         
   
     </div>
