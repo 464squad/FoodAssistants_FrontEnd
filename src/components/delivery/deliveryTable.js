5c87aa9f23132322a013d629608a1bfafa41ecd8
Richard C <112592860+OneDrahcirC@users.noreply.github.com>
2022-12-13 08:43:27 -0500

    corrected a mistake

--- src/components/delivery/deliveryTable.js
+++ src/components/delivery/deliveryTable.js
@@ -1,5 +1,31 @@
 function deliveryTable() {
-    return <div>This should be our deliveryTable component!</div>
-  }
-  
+    return ( <div className="app-container">
+    <h1> Delivery Table</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name, # in Home</th>
+          <th>Grocery Items</th>
+          <th>General Hygiene</th>
+          <th>Feminine Hygiene</th>
+          <th>Cleaning/Health Supplies</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {/* this is where the data goes */}
+          <td>Lameisha 3 people in house hold</td>
+          <td>chicken (1x)</td>
+          <td>Deodorant(3x)</td>
+          <td>Feminine Wipes (1x)</td>
+          <td>All purpose Cleaner (1x)</td>
+        </tr>
+      </tbody>
+    </table>
+
+
+
+
+  </div>)
+}
   export default deliveryTable
