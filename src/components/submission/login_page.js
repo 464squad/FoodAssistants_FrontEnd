f466f16d85c08f967753e21d87e6acfe9a420642
Allan Alvarez <88692955+Silverthefreak@users.noreply.github.com>
2022-12-21 16:32:38 -0500

    Update login_page.js

--- src/components/submission/login_page.js
+++ src/components/submission/login_page.js
@@ -1,11 +1,11 @@
 const login_page_component = () => {
     return(
         <form className = "login">
-            
+            <text placeholder = "Login" />
             <input placeholder = "Enter Username"/>
             <input placeholder = "Enter Password"/>
-            <input placeholder = "Login"/>
-            <input placeholder = "Forgot Password?"/>
+            <button placeholder = "Login"/>
+            <button placeholder = "Forgot Password?"/>
         </form>
     )
 }
